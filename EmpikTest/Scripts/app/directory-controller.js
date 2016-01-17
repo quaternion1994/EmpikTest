7f711ce1c943e650d0340a6acdc94b13f8f4e028
Vlad Petrenko <quaternion94@hotmail.com>
2016-01-18 00:59:48 +0200

    Более точное исправление

--- EmpikTest/Scripts/app/directory-controller.js
+++ EmpikTest/Scripts/app/directory-controller.js
@@ -14,20 +14,23 @@ app.controller('NavController', function ($scope, $http) {
             }
         }
         if (file.isFile == true) return;
-        $scope.currentPath = file.path;
         $http.get('api/Directory/GetList', { params: { path: file.path } }).success(function (data) {
+
                 $scope.filelist = data;
+                $scope.currentPath = file.path;
+
+                $http.get('api/Directory/GetAmounts', { params: { path: file.path } }).success(function (data) {
+                if (file.path == $scope.currentPath)
+                    $scope.amounts = data;
+                }).error(function (data, status) {
+                    if (file.path == $scope.currentPath)
+                        $scope.amounts = [0, 0, 0];
+                    $scope.messages.push(data);
+                });
+
         }).error(function (data, status) {
             $scope.messages.push(data);
         });
-        $http.get('api/Directory/GetAmounts', { params: { path: file.path } }).success(function (data) {
-            if (file.path == $scope.currentPath)
-                $scope.amounts = data;
-        }).error(function (data, status) {
-            if (file.path == $scope.currentPath)
-                $scope.amounts = [0, 0, 0];
-            $scope.messages.push(data);
-    });
     }
     $scope.navigate();
 })
